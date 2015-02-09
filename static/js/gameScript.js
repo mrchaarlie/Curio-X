c6bf030d7492cd49d9fc7ebbdbad07eeff6fd906
oakaomon <chanckenneth@gmail.com>
2015-02-09 03:08:15 -0500

    Circle drawing functionality implemented

--- static/js/gameScript.js
+++ static/js/gameScript.js
@@ -1,44 +1,54 @@
-
 $(document).ready(function (e){
-	// randomize which game mode it is based on session
-	// between classification->counting
-	//		   counting->classification
-	//		   randomized
-	// context reflects which mode
-	//
-	// 
+	var cArray = []
+	var radius = 15;
+	var canvas = document.getElementById('circleCanvas')
+	var ctx = canvas.getContext('2d');
+	var img = new Image();
+	// img.src = document.getElementById('testImage');
+	// img.onload = function(){
+		// ctx.drawImage(img,0,0)
+		// console.log('drawing image')
+	// }
+	
+	$('#circleCanvas').click(function (e) {
+		var posX = $(this).offset().left,
+			posY = $(this).offset().top;
+		
+		var coord = {x : (e.pageX - posX) , y : (e.pageY - posY)};
+		
+		cArrayUpdate(coord);
+		drawCircles();
+		console.log('coordsArray', cArray)
+	});
+	
+	function cArrayUpdate(newCoord) {
+		var noOverlap = true;
+		$.each(cArray, function(i, coord) {
+			if (isClose(newCoord, coord)) {
+				cArray.splice(i,1);
+				return noOverlap = false;
+			}
+		});
+		if(noOverlap) { cArray.push(newCoord) }
+	}
+	
+	function isClose(p1, p2){
+		var dx = p2.x-p1.x;
+		var dy = p2.y-p1.y;
+		return (Math.sqrt(dx*dx + dy*dy) <= radius);
+	}
 
-	var coordArray = []
-
-	$('#testImage').click(function (e) { //Offset mouse Position
-        var posX = $(this).offset().left,
-            posY = $(this).offset().top;
-            coordArray.push({x : (e.pageX - posX) , y : (e.pageY - posY)})
-            drawCircle(coordArray[coordArray.length - 1], coordArray.length)
-            // each circle is a canvas element
-            // TODO: need to delete circle when it is clicked, and delete associated coordinates in coordArray
-            // TODO: post coordArray to persist when next image is requested
-    });
-
-    function drawCircle(data, index) {
-		var elementID = 'circle' + index;
-	    $('<canvas>').attr({
-	        id: elementID
-	    }).css({
-	        width: 20 + 'px',
-	        height: 20 + 'px',
-	        left: data.x - 10,
-	        top : data.y - 10
-	    }).appendTo('#panzoomParent');
-
-	    var ctx = document.getElementById(elementID);
-	    ctx = ctx.getContext('2d')
-
-    	console.log(data)
-    	ctx.beginPath();
-	    ctx.arc(10, 10, 10, 0, Math.PI * 2); // draws a circle
-	    ctx.lineWidth = 2;
-	    ctx.stroke();
+	
+	function drawCircles(){
+		console.log('drawing circles');
+		ctx.clearRect(0,0, canvas.width, canvas.height);
+		$.each(cArray, function(i, data) {
+			ctx.beginPath();
+			ctx.arc(data.x, data.y, radius, 0, Math.PI * 2);
+			ctx.lineWidth = 2;
+			ctx.strokeStyle = 'red';
+			ctx.stroke();
+		});
 	}
 })
 
