2b2f446e5e29228cf4fea67b90a5b1f8f041d5b6
jd3johns <jd3johns@uwaterloo.ca>
2015-02-08 22:43:06 -0500

    Remove an import of a non-existent model

--- game/views.py
+++ game/views.py
@@ -8,28 +8,11 @@ from django.contrib.auth.decorators import login_required
 import os
 import datetime
 
-from game.models import Image
-
-current_dir = os.path.dirname(__file__)  # get current directory
+#current_dir = os.path.dirname(__file__)  # get current directory
 
 @login_required
 def index(request):
-    '''
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(username=username, password=password)
-    if user is not None:
-        if user.is_active:
-            login(request, user)'''
     return render_to_response('index.html', locals())
-    '''    else:
-            print('Disabled account')
-    else:
-        print('Invalid login')'''
 
 def game(request):
     return render_to_response('game.html', locals()) 
-
-#def game(request):
-#    image = Image.objects.get(id=1)
-#    return render_to_response('game.html',locals()) 
