265b34dad52bac6bc70d149bd457121bd3563a64
jd3johns <jd3johns@uwaterloo.ca>
2015-03-06 17:55:17 -0500

    Add data submit feature for game 1 and 2, albeit no data is being stored at the moment. Fixed the style.css to be un-broken pending proper fix.

--- game/urls.py
+++ game/urls.py
@@ -8,4 +8,6 @@ urlpatterns = patterns('',
     url(r'^game2$', views.game2, name='game2'),
     url(r'^game-splash$', views.gameSplash, name='game-splash'),
     url(r'^game2-splash$', views.game2Splash, name='game2-splash'),
+    url(r'^post$', views.game_submit_task, name='submit'),
+    url(r'^post2$', views.game2_submit_task, name='submit2'),
 )
--- game/views.py
+++ game/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, render_to_response
 from django.template.loader import get_template
 from django.template import Context
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseServerError
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.core.context_processors import csrf
 
 import os
 import logging
@@ -17,13 +18,17 @@ def index(request):
 
 #@login_required
 def game(request):
+    c = {}
+    c.update(csrf(request))
     logger.debug('Serve game mode 1 page')
-    return render_to_response('game.html', locals()) 
+    return render_to_response('game.html', c) 
 
 #@login_required
 def game2(request):
+    c = {}
+    c.update(csrf(request))
     logger.debug('Serve game mode 2 page')
-    return render_to_response('game2.html', locals()) 
+    return render_to_response('game2.html', c) 
 
 #@login_required
 def gameSplash(request):
@@ -35,10 +40,23 @@ def game2Splash(request):
     logger.debug('Serve game mode 2 splash')
     return render_to_response('game2-splash.html', locals()) 
 
+def game_submit_task(request):
+    if request.method == "POST":
+        post = request.POST.copy()
+        logger.debug(request)
+        c = {}
+        c.update(csrf(request))
+        logger.debug("Get the post from the game")
+        return render_to_response('game.html', c)
+    else:
+        return HttpResponseServerError("post error: not a post")
+
 def game2_submit_task(request):
     if request.method == "POST":
         post = request.POST.copy()
-        # do with post what you will
-        return render_to_response('game2.html', locals())
+        c = {}
+        c.update(csrf(request))
+        logger.debug("Get the post from game 2")
+        return render_to_response('game2.html', c)
     else:
         return HttpResponseServerError("post error: not a post")
